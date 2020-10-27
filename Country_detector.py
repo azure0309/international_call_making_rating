fe38325d50cdddb16d55dd821ae343c28c1f7a65
azure0309 <lifeofazure@gmail.com>
2020-10-27 17:55:40 +0800

    Added method that get info of country at Country_detector.py
    next feature is get informations of all countries and put it in dictionary specifically Dataframe to set easy to use later
    
    #1.1.0

--- Country_detector.py
+++ Country_detector.py
@@ -1,35 +1,50 @@
 from basic import Country
 import connect_ufms
 
+
 class Connection_to_UFMS():
 	def __init__(self, query):
-		self.query= query
+		self.query = query
 
 	def query_executor(self):
 		cursor = connect_ufms.connection.cursor()
 		execute = cursor.execute(self.query)
 		return execute
 
-class All_Country_List(Country):
-	query = 'select * from T_ALL_COUNTRY_CODE'
-	select_all_country_code = Connection_to_UFMS(query)
-	select_data = select_all_country_code.query_executor()
 
-	def __init__(self, name, rating):
-		super().__init__(self, name, rating)
+class Get_Country_Info(Country):
+	def __init__(self, country_code):
+		Country.__init__(self, country_code, country_code, country_code)
 
-		print(self.name)
+	def select_data_of_country(self):
+		query = f"select * from T_ALL_COUNTRY_CODE where COUNTRYCODE = '{self.country_code}'"
+		select_country_info = Connection_to_UFMS(query)
+		select_data = select_country_info.query_executor()
 
-	# def compress_selected_data(self):
+		number_info = {
+			'Continent': '',
+			'Country': '',
+			'Country_code': '',
+			'Alpha2': '',
+			'Alpha3': ''}
 
-my_country = All_Country_List('Mongolia', 3.5)
+		for value in select_data:
+			number_info['Continent'] = value[0]
+			number_info['Country'] = value[1]
+			number_info['Country_code'] = value[2]
+			number_info['Alpha2'] = value[3]
+			number_info['Alpha3'] = value[4]
+
+		return number_info
+
+	def select_all_countries(self):
+		query = "select * from T_ALL_COUNTRY_CODE"
+		select_country_info = Connection_to_UFMS(query)
+		select_data = select_country_info.query_executor()
 
 
 
-# query = 'select * from od_alert'
-# my_query = Connection_to_UFMS(query)
-# select = my_query.query_executor()
-#
-# for i in select:
-# 	print(i)
 
+hujaa = Get_Country_Info(82)
+hujaa_data = hujaa.select_data_of_country()
+print(hujaa_data)
